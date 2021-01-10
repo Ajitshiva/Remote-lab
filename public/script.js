aec74bdbd2738318e02d806b1611e6432889f5e6
Ajitshiva <ajitshiva007@gmail.com>
2021-01-11 04:02:19 +0530

    Compiler Added

--- public/script.js
+++ public/script.js
@@ -3,31 +3,120 @@ const name = prompt("Enter your name");
 let socket = io('/');
 ace.require("ace/keybindings/sublime");
 ace.require("ace/ext/language_tools");
+
 const editor = ace.edit("editor");
 editor.session.setMode("ace/mode/python");
 editor.setTheme("ace/theme/monokai");
 editor.setFontSize(16);
 editor.setShowPrintMargin(false);
 editor.setKeyboardHandler("ace/keyboard/sublime");
-// editor.setOption("enableBasicAutocompletion", true);
+
+const input = ace.edit("input");
+input.session.setMode("ace/mode/text");
+input.setTheme("ace/theme/monokai");
+input.setFontSize(16);
+input.setShowPrintMargin(false);
+input.setKeyboardHandler("ace/keyboard/sublime");
+
+const output = ace.edit("output");
+output.session.setMode("ace/mode/text");
+output.setTheme("ace/theme/monokai");
+output.setFontSize(16);
+output.setShowPrintMargin(false);
+output.setKeyboardHandler("ace/keyboard/sublime");
+
 editor.setOptions({
     enableBasicAutocompletion: true,
     enableLiveAutocompletion: true
 });
 
+const sampleCode = {
+    "c": "#include <stdio.h>\n\nint main() {\n\t// your code goes here\n\tprintf(\"Hello world\");\n\treturn 0;\n}\n\n",
+    "c++":"#include <iostream>\nusing namespace std;\n\nint main() {\n\t// your code goes here\n\tcout << \"Hello World\";\n\treturn 0;\n}\n",
+    "java":"import java.util.*;\nimport java.lang.*;\nimport java.io.*;\n\npublic class RemoteLab\n{\n\tpublic static void main (String[] args) throws java.lang.Exception\n\t{\n\t\t// your code goes here\n\t\tSystem.out.println(\"Hello world\");\n\t}\n}\n",
+    "javascript":"console.log(\"hello world\");",
+    "python":"import sys\nprint(\"hello world\")"   
+}
+editor.setValue(sampleCode["python"]);
+editor.clearSelection();
+
+let run = document.getElementById('run');
+run.addEventListener('click', ()=>{
+    let editor = ace.edit("editor");
+    let output = ace.edit("output");
+    let input = ace.edit("input");
+    let languageSelect = document.getElementById('language-select');
+    var language = languageSelect.value.toLowerCase();
+    var version = "3";
+    if(language == 'c' || language == 'c++') language = "cpp17", version = "0";
+    if(language == 'python') language = "python3";
+    if(language == 'javascript') language = "nodejs";
+    axios.get('/compile', {
+        params: {
+            script: editor.getValue(),
+            stdin: input.getValue(),
+            language: language,
+            versionIndex: version
+        }
+    })
+        .then(function (response) {
+            output.setValue(response.data.output);
+            output.clearSelection();
+            const code = output.getValue();
+            socket.emit('output-update', code);
+        })
+        .catch(function (error) {
+            output.setValue(error.data.output);
+            output.clearSelection();
+            const code = output.getValue();
+            socket.emit('output-update', code);
+        })
+        .then(function () {
+        });
+})
+
 const editorDiv = document.getElementById('editor');
+const inputDiv = document.getElementById('input');
+const outputDiv = document.getElementById('output');
 
 editorDiv.addEventListener('keyup', event => {
     const code = editor.getValue();
     socket.emit('code-update', code);
 });
+inputDiv.addEventListener('keyup', event => {
+    const code = input.getValue();
+    socket.emit('input-update', code);
+});
 
+
+const chatHeader = document.querySelector('.main__header');
+chatHeader.addEventListener('click', ()=>{
+let chatBody = document.getElementById('chat');
+chatBody.classList.toggle("open");
+});
+
+const meetingId = document.getElementById('meeting-id');
+meetingId.textContent = "Room Id: " + ROOM_ID;
+console.log("<%=roomId%>");
+
+
+meetingId.addEventListener('click', ()=>{
+    const meetingId = document.getElementById('meeting-id');
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(meetingId).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+})
 const languageSelect = document.getElementById('language-select');
 languageSelect.addEventListener('change', event => {
     var language = languageSelect.value;
     var mode = language;
     if(language === 'C' || language === 'C++')  mode = "c_cpp";
-    console.log(language.toLowerCase());
+    editor.setValue(sampleCode[language.toLowerCase()]);
+    editor.clearSelection();
+    socket.emit('code-update', sampleCode[language.toLowerCase()]);
     
     editor.session.setMode('ace/mode/' + mode.toLowerCase());
     socket.emit('language-change', language);
@@ -37,12 +126,19 @@ socket.on('code-update', code => {
     editor.setValue(code)
     editor.clearSelection();
 });
+socket.on('input-update', code => {
+    input.setValue(code)
+    input.clearSelection();
+});
+socket.on('output-update', code => {
+    output.setValue(code)
+    output.clearSelection();
+});
 
 socket.on('language-change',language => {
     languageSelect.value = language;
     var mode = language;
     if(language === 'C' || language === 'C++')  mode = "c_cpp";
-    console.log(mode.toLowerCase());
     editor.session.setMode('ace/mode/' + mode.toLowerCase());
 });
 
@@ -117,13 +213,13 @@ let text =$('input');
 $('html').keydown((e) => {
     if(e.which == 13 && text.val().length !== 0)
     {
-        console.log(text.val())
         socket.emit('message', text.val());
         text.val('');
     }
 })
 
-socket.on('joinMessage', username =>{
+socket.on('joinMessage', (username, noOfUsers) =>{
+    document.getElementById('no-of-users').textContent = noOfUsers;
     $('ul').append(`<li class = "join-message"><b>${username}</b> joined the room</li>`);
     scrollToBottom();
 })
@@ -131,7 +227,8 @@ socket.on('createMessage', (username, message) =>{
     $('ul').append(`<li class = "message"><b>${username}</b><br/>${message}</li>`);
     scrollToBottom();
 })
-socket.on('leftMessage', username =>{
+socket.on('leftMessage', (username, noOfUsers) =>{
+    document.getElementById('no-of-users').textContent = noOfUsers;
     $('ul').append(`<li class = "left-message"><b>${username}</b> left the room</li>`);
     scrollToBottom();
 })
@@ -160,13 +257,11 @@ const setMuteButton = () =>{
     const btn = `<i class = "fas fa-microphone"></i><span>Mute</span>`;
     var mute_btn = document.getElementById('mute__audio');
     mute_btn.innerHTML = btn;
-    console.log(mute_btn.innerHTML);
 }
 const setUnmuteButton = () =>{
     const btn = `<i class = "unmute fas fa-microphone-slash"></i><span>Unmute</span>`;
     var mute_btn = document.getElementById('mute__audio');
     mute_btn.innerHTML = btn;
-    console.log(mute_btn.innerHTML);
 }
 
 
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 
 const express = require('express');
 const app = express();
+const axios = require('axios');
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const {v4 : uuidv4} = require('uuid');
 const {ExpressPeerServer} = require('peer');
-const { send } = require('process');
 const peerServer = ExpressPeerServer(server, {
     debug : true
 });
 
 
 let currentCode = "";
+let currentInput = "";
+let currentOutput = "";
 let currentLanguage = "Python";
-
+let noOfUsers = 0;
 app.set('view engine', 'ejs');
 app.use('/peerjs', peerServer);
 app.use(express.static('public'))
@@ -23,6 +25,26 @@ app.get('/', (req, res)=>{
 app.get('/left', (req, res)=>{
     res.send("Thank you for using this app. Please visit again");
 })
+app.get('/compile', (req, res)=>{
+    const stmt = {
+        clientId: "977785f5398e546e3d5f4060e34807da",
+        clientSecret:"e90c69b841610cc183b8a2057c56bdfbf881313d676b0d1e958155ab4c28c76",
+        script : req.query.script ,
+        stdin: req.query.stdin,
+        language: req.query.language,
+        versionIndex: req.query.versionIndex,
+        
+    };
+    axios.post('https://api.jdoodle.com/v1/execute', stmt)
+        .then(function (response) {
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            res.send(error.data)
+        })
+
+})
+
 app.get('/:room', (req, res)=>{
     res.render('room', {roomId: req.params.room});
 })
@@ -31,10 +53,10 @@ io.on('connection', socket => {
     
     socket.on('join-room', (roomId, userId, username)=>{
         socket.join(roomId);
-        
+        noOfUsers++;
         console.log(username + " connected.");
         socket.to(roomId).broadcast.emit("user-connected", userId);
-        io.to(roomId).emit('joinMessage', username);
+        io.to(roomId).emit('joinMessage', username, noOfUsers);
         socket.on('message', message => {
             io.to(roomId).emit('createMessage', username, message);
         })
@@ -45,13 +67,22 @@ io.on('connection', socket => {
             currentCode = code;
             socket.to(roomId).broadcast.emit('code-update', code);
         })
+        socket.on('input-update', input =>{
+            currentInput = input;
+            socket.to(roomId).broadcast.emit('input-update', input);
+        })
+        socket.on('output-update', output =>{
+            currentOutput = output;
+            socket.to(roomId).broadcast.emit('output-update', output);
+        })
         socket.on('language-change', language =>{
             currentLanguage = language;
             socket.to(roomId).broadcast.emit('language-change', language);
         })
         socket.on('disconnect', ()=>{
+            noOfUsers--;
             console.log(username + " disconnected");
-            io.to(roomId).emit('leftMessage', username);
+            io.to(roomId).emit('leftMessage', username, noOfUsers);
             socket.to(roomId).broadcast.emit('user-disconnected', userId);
         })
     
